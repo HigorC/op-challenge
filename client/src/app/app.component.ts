37c398544a8063781064dafb282df1986768cd31
HigorC <higor_oliveira@outlook.com>
2022-10-30 17:11:11 +0000

    feat: pagination

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { APIResult } from './interfaces/APIResult.interface';
+import { setParameter } from './utils/url';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  baseAPIUrl = 'http://localhost:8080/v1'
+  page: Number = 0;
+  pageSize: Number = 10;
+  query: string = '';
+  pages: Array<Number> = []
+
+  result: APIResult | undefined;
+
+  constructor(private http: HttpClient) {
+  }
+
+  paginate(page: Number) {
+    this.page = page
+    this.search()
+  }
+
+  nextPage() {
+    if (this.result) {
+      this.paginate(this.result.currentPage + 1)
+    }
+  }
+
+  previousPage() {
+    if (this.result) {
+      this.paginate(this.result.currentPage - 1)
+    }
+  }
+
+  search() {
+    let url = `${this.baseAPIUrl}/beers?`
+
+    if (this.query) {
+      url = setParameter(url, 'query', this.query)
+    }
+    url = setParameter(url, 'page', String(this.page))
+    url = setParameter(url, 'pageSize', String(this.pageSize))
+
+    this.http
+      .get<APIResult>(url)
+      .subscribe((data: APIResult) => {
+        this.result = data
+        this.result.beers.map(beer => {
+          const maxLength = 100
+          if (beer.description.length >= maxLength) {
+            beer.description = beer.description.slice(0, maxLength) + "..."
+          }
+        })
+        this.buildPagination()
+      })
+  }
+
+  buildPagination() {
+    this.pages = []
+    if (this.result) {
+      this.pages.push(this.result.currentPage)
+
+      for (let i = this.result.currentPage - 1; i >= 0 && i >= this.result.currentPage - 3; i--) {
+        this.pages.unshift(i)
+      }
+
+      for (let i = this.result.currentPage + 1; i <= this.result.totalPages - 1 && i <= this.result.currentPage + 3; i++) {
+        this.pages.push(i)
+      }
+    }
+  }
+
+  async ngOnInit() {
+    this.search()
+  }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { BeerComponent } from './beer/beer.component';
+import { SearchInputComponent } from './search-input/search-input.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BeerComponent,
+    SearchInputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
